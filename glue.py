719848aa985d94b70e92f229a40417c381bd70ec
yuqian5 <Yg[FaXCDmQuMgJF(g8SseEMuiCF4>
2020-11-29 16:14:48 -0700

    sync

--- glue.py
+++ glue.py
@@ -1,22 +1,48 @@
 import agent
-# TODO
-# import env
+import MontainCarEnv
 
 class glue:
     def __init__(self):
-        self.agent = agent.ExpectedSarsaAgent()
-        self.agent.agent_init()
+        self.agent = None
+        self.env = MontainCarEnv.MountainCar()
 
-    '''
-    The gist is...
-    glue will start and create an instance of the agent and env
-    
-    then:
-    
-    glue call env to get start state
-    glue call agent start with start state and get action
-    
-    loop:
-    glue use action to call env, get reward and new state (position, velocity)
-    glue uses (position, velocity) to call agent_step to get new action
-    '''
+        self.w = None
+
+    def run(self, episode_count):
+        run_count = 0
+
+        while run_count < episode_count:
+            if self.w:
+                agent_info = {"initial_weights": self.w}
+                self.agent = agent.ExpectedSarsaAgent()
+                self.agent.agent_init(agent_info)
+            else:
+                self.agent = agent.ExpectedSarsaAgent()
+                self.agent.agent_init()
+
+            self.run_episode(run_count)
+            run_count += 1
+
+    def run_episode(self, episode_num):
+        state = self.env.state
+        action = None
+        reward = None
+
+        action = self.agent.agent_start(state)
+        while True:
+            state, reward, done = self.env.step(action)
+            if done:
+                self.w = self.agent.w
+                self.env.render(file_path="./mountainCar" + episode_num + ".mp4", mode='mp4')
+                self.env.reset()
+                return 0
+            else:
+                action = self.agent.agent_step(reward, state)
+
+def main():
+    g = glue()
+    g.run(5)
+
+if __name__ == '__main__':
+    main()
+
